458e14187dd5a142198b5da3edb3f827c429d3d4
IshimweSamuel <ishimweniyishoborasamuel@gmail.com>
2023-03-16 14:58:32 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - a command that prints a line with puts funcion
+ *Return: 0 (success)
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle ");
+  return (0);
+    }
