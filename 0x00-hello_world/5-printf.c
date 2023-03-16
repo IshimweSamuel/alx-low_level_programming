706c6f8ea867e64f12d42f48eeb3e95c2ef3176c
IshimweSamuel <ishimweniyishoborasamuel@gmail.com>
2023-03-16 15:11:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.sh>
+/**
+ *main - A program that prints an output using printf funtion
+ *return: "0" (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
