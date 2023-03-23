95a25399fb2bcce84bd7d9c6c4327f0d6bc250ac
IshimweSamuel <ishimweniyishoborasamuel@gmail.com>
2023-03-23 05:32:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main: a program that prints the sizes of different data types
- *return '0' (Success)
+ *main - a program that prints the sizes of different data types
+ *return 0 (Success)
  */
 int main(void)
 {
